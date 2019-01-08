7e73dabc3d25488444f53778d3c7141aa3a01fb6
Braden Shipley <bradenrshipley@gmail.com>
2019-01-08 14:47:15 -0600

    add solutions

--- stage-4/src/components/StoreFront/StoreFront.js
+++ stage-4/src/components/StoreFront/StoreFront.js
@@ -9,24 +9,22 @@ class StoreFront extends Component {
   }
 
   render() {
-    let productDisplay = this.props.products.map((element, index) => {
-      return (
-        <div className='product-container' key={index}>
-          <h2>{element.title}</h2>
-          <img src={element.image} alt='' />
-          <h2>{element.desc}</h2>
-          <h3>{"$" + element.price + ".00"}</h3>
-          <button onClick={() => this.props.addToShoppingCart(element)}>
-            Purchase!
-          </button>
-        </div>
-      )
-    })
-    return (
-      <div className='storefront-container'>
-        {this.props.products && productDisplay}
-      </div>
-    )
+    let productDisplay =
+      this.props.products &&
+      this.props.products.map((element, index) => {
+        return (
+          <div className='product-container' key={index}>
+            <h2>{element.title}</h2>
+            <img src={element.image} alt='' />
+            <h2>{element.desc}</h2>
+            <h3>{"$" + element.price + ".00"}</h3>
+            <button onClick={() => this.props.addToShoppingCart(element)}>
+              Purchase!
+            </button>
+          </div>
+        )
+      })
+    return <div className='storefront-container'>{productDisplay}</div>
   }
 }
 
